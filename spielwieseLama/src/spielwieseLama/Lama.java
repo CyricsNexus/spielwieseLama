2831f7cfc2a956a1ea57e79e86089b96d5048b0e
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-07-25 12:01:37 +0200

    Lama bekommt Namen

--- spielwieseLama/src/spielwieseLama/Lama.java
+++ spielwieseLama/src/spielwieseLama/Lama.java
@@ -2,8 +2,8 @@ package spielwieseLama;
 
 public class Lama {
 	
-	private String rufname;
-	private String geraeusch = "Möööht";
+	private String rufname = "Carl";
+	private String geraeusch;
 	
 	public Lama(String rufname, String gerauesch) {
 		this.rufname = rufname;
