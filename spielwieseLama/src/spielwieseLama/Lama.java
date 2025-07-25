a4e721687089237970cf04f7c7d1f9a7fdd9683e
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-07-25 12:48:20 +0200

    Mopple + Geräusch

--- spielwieseLama/src/spielwieseLama/Lama.java
+++ spielwieseLama/src/spielwieseLama/Lama.java
@@ -3,7 +3,7 @@ package spielwieseLama;
 public class Lama {
 	
 	private String rufname = "Mopple";
-	private String geraeusch;
+	private String geraeusch = "Möööht";
 	
 	public Lama(String rufname, String gerauesch) {
 		this.rufname = rufname;
