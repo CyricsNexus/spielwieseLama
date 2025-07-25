809812a3984a6dfd5e92bdcd5376a7a4dd493b16
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-07-25 12:20:19 +0200

    Lama aktualisiert

--- spielwieseLama/src/spielwieseLama/Lama.java
+++ spielwieseLama/src/spielwieseLama/Lama.java
@@ -2,8 +2,8 @@ package spielwieseLama;
 
 public class Lama {
 	
-	private String rufname = "Aber Caaaaarl";
-	private String geraeusch;
+	private String rufname = "Carl";
+	private String geraeusch = "MöööhtMööht";
 	
 	public Lama(String rufname, String gerauesch) {
 		this.rufname = rufname;
