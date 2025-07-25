ba696c152cee6bb26f51ac8abaf406f3cba70d29
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-07-25 12:46:40 +0200

    Mopple2Go

--- spielwieseLama/src/spielwieseLama/Lama.java
+++ spielwieseLama/src/spielwieseLama/Lama.java
@@ -2,7 +2,7 @@ package spielwieseLama;
 
 public class Lama {
 	
-	private String rufname = "Maple";
+	private String rufname = "Mopple";
 	private String geraeusch;
 	
 	public Lama(String rufname, String gerauesch) {
