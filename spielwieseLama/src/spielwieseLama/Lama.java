e61f798f734c2121e5a3c974ccb7b3a2e0c961f0
CFRA204 <CFRA204@PC42192191.cc-college.gecs.loc>
2025-07-25 12:45:04 +0200

    Mein neues Lama

--- spielwieseLama/src/spielwieseLama/Lama.java
+++ spielwieseLama/src/spielwieseLama/Lama.java
@@ -2,8 +2,8 @@ package spielwieseLama;
 
 public class Lama {
 	
-	private String rufname = "Carl";
-	private String geraeusch = "Möööht";
+	private String rufname = "Maple";
+	private String geraeusch;
 	
 	public Lama(String rufname, String gerauesch) {
 		this.rufname = rufname;
