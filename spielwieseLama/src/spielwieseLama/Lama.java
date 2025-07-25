7d167745f5130a29c43bfbaf236c8808976f4b4a
CyricsNexus <68751459+CyricsNexus@users.noreply.github.com>
2025-07-25 11:39:36 +0200

    Update Lama.java

--- spielwieseLama/src/spielwieseLama/Lama.java
+++ spielwieseLama/src/spielwieseLama/Lama.java
@@ -3,7 +3,7 @@ package spielwieseLama;
 public class Lama {
 	
 	private String rufname;
-	private String geraeusch = "Möööht";
+	private String geraeusch = "MöööhtMööht";
 	
 	public Lama(String rufname, String gerauesch) {
 		this.rufname = rufname;
